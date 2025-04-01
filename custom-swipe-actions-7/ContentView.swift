924a0be225320e95505b9bf1099d045f08aa7f28
kopyl <oleggggg1997@gmail.com>
2025-04-01 22:05:23 +0300

    Initial Commit

---
+++ custom-swipe-actions-7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  custom-swipe-actions-7
+//
+//  Created by Oleh Kopyl on 01.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ custom-swipe-actions-7/custom_swipe_actions_7App.swift
@@ -0,0 +1,17 @@
+//
+//  custom_swipe_actions_7App.swift
+//  custom-swipe-actions-7
+//
+//  Created by Oleh Kopyl on 01.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct custom_swipe_actions_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
